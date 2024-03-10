cb70dfd58bf8666b83fc38ed08121bf61a311240
John <johnolshea@gmail.com>
2024-03-10 16:10:26 -0700

    9. More classes! and Test for user

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Test module"""
+import unittest
+import models
+from models.state import State
+from datetime import datetime
+
+
+class TestStateModel(unittest.TestCase):
+    """Unittest for testing State class"""
+
+    def test_no_args(self):
+        self.assertEqual(State, type(State()))
+
+    def test_id(self):
+        self.assertEqual(str, type(State().id))
+
+    def test_created_at(self):
+        self.assertEqual(datetime, type(State().created_at))
+
+    def test_updated_at(self):
+        self.assertEqual(datetime, type(State().updated_at))
+
+    def test_name(self):
+        new_state = State()
+        self.assertEqual(str, type(State().name))
+        self.assertNotEqual("name", new_state.__dict__)
+        self.assertIn("name", dir(new_state))
+
+    def test_two_state(self):
+        new_state1 = State()
+        new_state2 = State()
+        self.assertNotEqual(new_state1.id, new_state2.id)
+
+
+class TestStateModel_to_dict(unittest.TestCase):
+    """Unittest for testing to_dict method of the State class"""
+
+    def test_to_dict(self):
+        self.assertTrue(dict, type(State().to_dict()))
+
+    def test_to_dict_keys(self):
+        new_state = State()
+        self.assertIn("id", new_state.to_dict())
+        self.assertIn("created_at", new_state.to_dict())
+        self.assertIn("updated_at", new_state.to_dict())
+        self.assertIn("__class__", new_state.to_dict())
+
+    def test_to_dict_attributes(self):
+        new_state = State()
+        new_state.my_number = 123
+        self.assertIn("my_number", new_state.to_dict())
+
+    def test_to_dict_datetime_str(self):
+        new_state = State()
+        state_dict = new_state.to_dict()
+        self.assertEqual(str, type(state_dict["created_at"]))
+        self.assertEqual(str, type(state_dict["updated_at"]))
+        self.assertEqual(str, type(state_dict["id"]))
+
+    def test_contrast_dict(self):
+        new_state = State()
+        self.assertNotEqual(new_state.to_dict(), new_state.__dict__)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Test module"""
+import unittest
+import models
+from models.user import User
+from datetime import datetime
+
+
+class TestUserModel(unittest.TestCase):
+    """Unittest for testing User class"""
+
+    def test_no_args(self):
+        self.assertEqual(User, type(User()))
+
+    def test_id(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at(self):
+        self.assertEqual(datetime, type(User().created_at))
+
+    def test_updated_at(self):
+        self.assertEqual(datetime, type(User().updated_at))
+
+    def test_email(self):
+        self.assertEqual(str, type(User().email))
+
+    def test_password(self):
+        self.assertEqual(str, type(User().password))
+
+    def test_first_name(self):
+        self.assertEqual(str, type(User().first_name))
+
+    def test_last_name(self):
+        self.assertEqual(str, type(User().last_name))
+
+    def test_two_user(self):
+        new_user1 = User()
+        new_user2 = User()
+        self.assertNotEqual(new_user1.id, new_user2.id)
+
+
+class TestUserModel_to_dict(unittest.TestCase):
+    """Unittest for testing to_dict method of the User class"""
+
+    def test_to_dict(self):
+        self.assertTrue(dict, type(User().to_dict()))
+
+    def test_to_dict_keys(self):
+        new_user = User()
+        self.assertIn("id", new_user.to_dict())
+        self.assertIn("created_at", new_user.to_dict())
+        self.assertIn("updated_at", new_user.to_dict())
+        self.assertIn("__class__", new_user.to_dict())
+
+    def test_to_dict_attributes(self):
+        new_user = User()
+        new_user.my_number = 123
+        self.assertIn("my_number", new_user.to_dict())
+
+    def test_to_dict_datetime_str(self):
+        new_user = User()
+        user_dict = new_user.to_dict()
+        self.assertEqual(str, type(user_dict["created_at"]))
+        self.assertEqual(str, type(user_dict["updated_at"]))
+        self.assertEqual(str, type(user_dict["id"]))
+
+    def test_contrast_dict(self):
+        new_user = User()
+        self.assertNotEqual(new_user.to_dict(), new_user.__dict__)
+
+
+if __name__ == "__main__":
+    unittest.main()
