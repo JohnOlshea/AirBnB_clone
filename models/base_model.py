2cfb0ad85e23925186cdd1a5dc8de1f71a94ab38
John <johnolshea@gmail.com>
2024-03-10 09:47:33 -0700

    fix pycodestyle compliant and add more tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-"""BaseModel class for all other models in the project.
-
-This class provides common attributes and methods for other models to inherit from.
+"""
+BaseModel class for all other models in the project.
 """
 
 import uuid
 from datetime import datetime
+
 
 class BaseModel:
     """
@@ -21,7 +20,7 @@ class BaseModel:
         Attributes:
             id (str): Unique identifier for the instance.
             created_at (datetime): Date and time when the instance is created.
-            updated_at (datetime): Date and time when the instance is last updated.
+            updated_at (datetime): When the instance is last updated.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -31,20 +30,25 @@ class BaseModel:
         """
         Returns a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
 
     def save(self):
         """
         Updates the 'updated_at' attribute with the current date and time.
-        """        
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary representation of the object with ISO formatted timestamps.
+        Returns a dictionary representation of the object
+        with ISO formatted timestamps.
 
         This dictionary can be used for serialization purposes.
-        """        
+        """
         model_dict = self.__dict__.copy()
         model_dict["__class__"] = self.__class__.__name__
         model_dict["created_at"] = model_dict["created_at"].isoformat()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,17 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Tests for the BaseModel class."""
 
+    def test_instantiation(self):
+        """Tests that a BaseModel instance is created with the correct attributes."""
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel);
+        self.assertTrue(hasattr(my_model, "id"))
+        self.assertTrue(hasattr(my_model, "created_at"))
+        self.assertTrue(hasattr(my_model, "updated_at"))
+        self.assertEqual(type(my_model.id), str)
+        self.assertEqual(type(my_model.created_at), datetime)
+        self.assertEqual(type(my_model.updated_at), datetime)
+
     def test_save(self):
         """
         Test the save method.
@@ -17,6 +28,21 @@ class TestBaseModel(unittest.TestCase):
         initial_updated_at = my_model.updated_at
         my_model.save()
         self.assertNotEqual(initial_updated_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """Test the to_dict method."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        model_dict = my_model.to_dict()
+        expected_keys = ['id', 'created_at', 'updated_at', '__class__', 'name', 'my_number']
+        self.assertEqual(sorted(model_dict.keys()), sorted(expected_keys))
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], my_model.id)
+        self.assertEqual(model_dict['name'], 'My First Model')
+        self.assertEqual(model_dict['my_number'], 89)
+        self.assertEqual(type(model_dict["created_at"]), str)
+        self.assertEqual(type(model_dict["updated_at"]), str)
 
 if __name__ == '__main__':
     unittest.main()        
